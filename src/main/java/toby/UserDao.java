4df2fb8d9f37e76235d3b86fdda83336d76dadcf
positoy <positoy@gmail.com>
2020-10-25 21:36:18 +0900

    1.1 초난감 DAO

---
+++ src/main/java/toby/UserDao.java
@@ -0,0 +1,60 @@
+package toby;
+
+import java.sql.*;
+
+public class UserDao {
+
+    final static String dbClass = "com.mysql.cj.jdbc.Driver";
+    final static String dbUrl = "jdbc:mysql://localhost/toby?serverTimezone=UTC&characterEncoding=UTF-8";
+    final static String dbUser = "root";
+    final static String dbPassword = "1234";
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName(dbClass);
+        Connection c = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+
+    public void delete(String id) throws ClassNotFoundException, SQLException {
+        Class.forName(dbClass);
+        Connection c = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+        PreparedStatement ps = c.prepareStatement("delete from users where id=?");
+        ps.setString(1, id);
+        ps.execute();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName(dbClass);
+        Connection c = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id=?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
---
+++ src/test/java/toby/UserDaoTest.java
@@ -0,0 +1,40 @@
+package toby;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void testUser() {
+        User user = new User();
+        user.setId("positoy");
+        user.setName("andy");
+        user.setPassword("10laskrcjs");
+
+        assertEquals(user.getId(), "positoy");
+        assertEquals(user.getName(), "andy");
+        assertEquals(user.getPassword(), "10laskrcjs");
+    }
+
+    @Test
+    void testUserDao() throws SQLException, ClassNotFoundException {
+
+        User user = new User();
+        user.setId("positoy");
+        user.setName("andy");
+        user.setPassword("10laskrcjs");
+
+        UserDao userDao = new UserDao();
+        userDao.delete("positoy");
+        userDao.add(user);
+        User storedUser = userDao.get("positoy");
+
+        assertEquals(user.getId(), storedUser.getId());
+        assertEquals(user.getName(), storedUser.getName());
+        assertEquals(user.getPassword(), storedUser.getPassword());
+    }
+}
