1a53dd25d678bb4f0b51d5e60ddae6846bd59814
positoy <positoy@gmail.com>
2020-10-26 23:43:01 +0900

    1.4.2 create common connectionMaker in DaoFactory

--- src/main/java/toby/DaoFactory.java
+++ src/main/java/toby/DaoFactory.java
@@ -1,7 +1,15 @@
 package toby;
 
 public class DaoFactory {
-    public UserDao userDao() {
-        return new UserDao(new JdbcConnectionMaker());
+    public ConnectionMaker connectionMaker() {
+        return new JdbcConnectionMaker();
     }
+
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
+    }
+
+//    public AccountDao accountDao() {
+//        return new AccountDao(connectionMaker());
+//    }
 }
